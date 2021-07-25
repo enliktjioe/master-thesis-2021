7ede6b8af055ff0c92d1813bff22656c6839b2e9
Enlik <enlik@ut.ee>
2021-07-26 02:43:04 +0300

    update utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -304,3 +304,79 @@ def topic_distribution_across_docs(df_topic_sents_keywords):
     df_dominant_topic.columns = ['Dominant_Topic', 'Keywords', 'Num_Documents', 'Perc_Documents']
 
     return df_dominant_topic
+
+    
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield (gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+
+class NLPpipe:
+    def __init__(self, bigram=True, sent_to_words=sent_to_words, remove_stop_words=remove_stopwords,
+                 make_bigrams=make_bigrams, make_trigrams=make_trigrams, lemmatize=lemmatize,
+                 create_dict=create_dictionary):
+        self.bigram = bigram
+        self.remove_things = remove_things
+        self.sent_to_words = sent_to_words
+        self.remove_stopwords = remove_stopwords
+        self.make_bigrams = make_bigrams
+        self.make_trigrams = make_trigrams
+        self.lemmatize = lemmatize
+        self.clean_text = None
+        self.create_dictionary = create_dictionary
+        self.dictionary = None
+        self.min_count = 5
+        self.threshold = .2
+        self.scoring = 'npmi'
+
+    def preprocess(self, documents):
+        """
+        Break sentences into words, remove punctuations and stopmake words, make bigrams, and lemmatize the documents
+        """
+        cleaned_docs = remove_things(documents)
+        lists_of_words = list(self.sent_to_words(cleaned_docs))
+        lists_of_words_no_stops = self.remove_stopwords(lists_of_words)
+        if self.bigram:
+            ngrams = self.make_bigrams(lists_of_words_no_stops, self.min_count, self.threshold, self.scoring)
+        else:
+            ngrams = self.make_trigrams(lists_of_words_no_stops, self.threshold, self.scoring)  # Need to fix parameters
+        data_lemmatized = self.lemmatize(ngrams, allowed_postags=['NOUN'])
+        return data_lemmatized
+
+    def fit(self, text, min_count=None, threshold=None, scoring=None):
+        """
+        Create a dictionary after preprocessing.
+        """
+        if min_count is not None: self.min_count = min_count
+        if threshold is not None: self.threshold = threshold
+        if scoring is not None: self.scoring = scoring
+
+        self.clean_text = self.preprocess(text)
+        self.dictionary = corpora.Dictionary(self.clean_text)
+
+    def transform(self, text, tf_idf=False):
+        """
+        Return a term-doc matrix using the fit dictionary
+        """
+        clean_text = self.preprocess(text)
+        term_doc = [self.dictionary.doc2bow(text) for text in clean_text]
+        if tf_idf:
+            return models.TfidfModel(term_doc, smartirs='ntc')[term_doc]
+        else:
+            return term_doc
+
+    def fit_transform(self, text, tf_idf=False, min_count=None, threshold=None, scoring=None):
+        """
+        Create a dictionary after preprocessing and return a term-doc matrix using the dictionary.
+        """
+        if min_count is not None: self.min_count = min_count
+        if threshold is not None: self.threshold = threshold
+        if scoring is not None: self.scoring = scoring
+
+        self.clean_text = self.preprocess(text)
+        self.dictionary = corpora.Dictionary(self.clean_text)
+        term_doc = [self.dictionary.doc2bow(text) for text in self.clean_text]
+        if tf_idf:
+            return models.TfidfModel(term_doc, smartirs='ntc')[term_doc]
+        else:
+            return term_doc
