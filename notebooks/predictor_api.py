409b163f860507f18e8af3ef6d7cc1155fe0f45c
Enlik <enlik@ut.ee>
2021-07-28 00:58:17 +0300

    update UI for flask app

--- notebooks/predictor_api.py
+++ notebooks/predictor_api.py
@@ -18,12 +18,14 @@ ldamallet = gensim.models.wrappers.ldamallet.malletmodel2ldamodel(ldamallet)
 
 # Import bert model
 # https://stackoverflow.com/questions/62678035/can-i-resume-training-from-checkpoints-using-the-ktrain-library
-# bert_model = ktrain.load_predictor('bert_model').model
-# bert_preproc = ktrain.load_predictor('bert_model').preproc
+# bert_model = ktrain.load_predictor('bert_model_all').model
+# bert_preproc = ktrain.load_predictor('bert_model_all').preproc
+# bert_model = ktrain.load_predictor('bert_model_all_p2').model
+# bert_preproc = ktrain.load_predictor('bert_model_all_p2').preproc
 bert_model = ktrain.load_predictor('bert_model_all_p3').model
 bert_preproc = ktrain.load_predictor('bert_model_all_p3').preproc
-# bert_model = ktrain.load_predictor('C:/GitRepo/master-thesis-2021/notebooks/bert_model_all').model
-# bert_preproc = ktrain.load_predictor('C:/GitRepo/master-thesis-2021/notebooks/bert_model_all').preproc
+# bert_model = ktrain.load_predictor('bert_model_all_p4').model
+# bert_preproc = ktrain.load_predictor('bert_model_all_p4').preproc
 
 bert_predictor = ktrain.get_predictor(bert_model, bert_preproc)
 
@@ -57,23 +59,25 @@ def make_prediction_bert(input):
     return None
 
 def make_suggestion(topic):
-    suggestion = None
-    if topic == "Platform/Device":
-        suggestion = "Your review may be about platforms or device. Please send an email to our Device Support team at " \
-                     "privacy@netflix.com or contact us at 1-(866)-579-7172. Thank you."
-    if topic == "User Experience":
-        suggestion = "If your review is about Account or Profile settings, please refer to our \"Manage my account\" at " \
-                     "https://help.netflix.com/en/ or contact us at 1-844-505-2993 for more information."
-    if topic == "Value":
-        suggestion = "Thank you for sharing your experience with Netflix! If you'd like to ask about programs, " \
-                     "subscriptions, costs, or anything else, please contact us at 1-844-505-2993."
-    if topic == "Service":
-        suggestion = "If you have any question about the services we provide, please contact Netflix Customer Service" \
-                     " through the app or call us at 1-(866)-579-7172."
-    if topic == "Trouble-shooting":
-        suggestion = "If this is about any inconvenience caused when using the app, we are so sorry. We will carefully read your" \
-                     " feedback and try to fix the issue as soon as possible."
-    if topic == "Shows":
-        suggestion = "Thank you for leaving a review about the programs/shows in Netflix. We value your experience with" \
-                     " our programs and it will be shared with our partners and film companies as well."
+    # suggestion = None
+    # if topic == "Platform/Device":
+    #     suggestion = "Your review may be about platforms or device. Please send an email to our Device Support team at " \
+    #                  "privacy@netflix.com or contact us at 1-(866)-579-7172. Thank you."
+    # if topic == "User Experience":
+    #     suggestion = "If your review is about Account or Profile settings, please refer to our \"Manage my account\" at " \
+    #                  "https://help.netflix.com/en/ or contact us at 1-844-505-2993 for more information."
+    # if topic == "Value":
+    #     suggestion = "Thank you for sharing your experience with Netflix! If you'd like to ask about programs, " \
+    #                  "subscriptions, costs, or anything else, please contact us at 1-844-505-2993."
+    # if topic == "Service":
+    #     suggestion = "If you have any question about the services we provide, please contact Netflix Customer Service" \
+    #                  " through the app or call us at 1-(866)-579-7172."
+    # if topic == "Trouble-shooting":
+    #     suggestion = "If this is about any inconvenience caused when using the app, we are so sorry. We will carefully read your" \
+    #                  " feedback and try to fix the issue as soon as possible."
+    # if topic == "Shows":
+    #     suggestion = "Thank you for leaving a review about the programs/shows in Netflix. We value your experience with" \
+    #                  " our programs and it will be shared with our partners and film companies as well."
+
+    suggestion = "Review Analysis"
     return suggestion
